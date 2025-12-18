7259fab148968e2262c2274c345bee9594899e5d
ALAN-KRATI <alankratisaxena@gmail.com>
2025-12-18 15:41:34 +0530

    [Alankrati] Added code for climbing stairs

---
+++ core-java-practice/leet-code-codebase/ClimbingStairs.java
@@ -0,0 +1,17 @@
+class ClimbingStairs{
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+       
+
+        int a = 1;
+        int b = 1;
+        
+        for(int i = 2; i <= n; i++){
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return b;
+    }
+}
