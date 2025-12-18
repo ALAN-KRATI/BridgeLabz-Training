1a9c5e407a4e46455789f0fcfcf54d9c79bb3ce0
ALAN-KRATI <alankratisaxena@gmail.com>
2025-12-18 14:03:32 +0530

    [Alankrati] calculated total amount for buying some items

---
+++ core-java-practice/gcr-codebase/programming-elements/Purchase.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Purchase {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int unitPri = sc.nextInt();
+        int quan = sc.nextInt();
+        int totalp = unitPrice * quantity;
+
+        System.out.println("The total purchase price is INR " + totalp + " if the quantity " + quan + " and unit price is INR " + unitPri);
+    }
+}
