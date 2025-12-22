9eef6f31fdad7437f9e19ae090bb0ad1fc618bf2
ALAN-KRATI <alankratisaxena@gmail.com>
2025-12-22 15:16:50 +0530

    [Alankrati] Add .Added code for Level-2 Problems

---
+++ core-java-practice/gcr-codebase/java-methods/NaturalSum.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class NaturalSum {
+  
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int rec = sumRecursive(n);
+        int form = sumFormula(n);
+        
+        System.out.println("Recursive Sum: " + rec);
+        System.out.println("Formula Sum: " + form);
+    }
+
+      public static int sumRecursive(int n) {
+        if (n <= 1) return n;
+        return n + sumRecursive(n - 1);
+    }
+
+    public static int sumFormula(int n) {
+        return n * (n + 1) / 2;
+    }
+}
